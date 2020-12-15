bce4b4d90bb16232023f56e2847d0a5a377ce0b5
Svilen Todorov <sv.ttodorov@gmail.com>
2020-12-15 23:57:41 +0200

    initial room design

---
+++ Assets/SwitchLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SwitchLight : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip lightSwitchClick;
+    public GameObject ceilingLight;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        if (hand.GetGrabStarting() != GrabTypes.None)
+        {
+            GetComponent<Animator>().SetTrigger("LightSwitchClicked");
+            audioSource.PlayOneShot(lightSwitchClick, 0.7F);
+            ceilingLight.SetActive(!ceilingLight.activeSelf);
+        }
+    }
+}
