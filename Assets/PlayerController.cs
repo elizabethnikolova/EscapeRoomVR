b405419bf807327247c07a86b55ec1aca7c0e62b
Svilen Todorov <sv.ttodorov@gmail.com>
2020-12-17 23:48:34 +0200

    Edit player object

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,13 +6,19 @@ using Valve.VR.InteractionSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public ISteamVR_Action_Vector2 input;
+    public SteamVR_Action_Vector2 input;
     public float speed = 1;
 
-    // Update is called once per frame
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);        
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
     }
 }
